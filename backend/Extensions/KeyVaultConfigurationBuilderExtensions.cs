97e291851149cc346c3fbc1d88085ed54d1985f3
Jeremiah Lipscomb <t-jelipscomb@microsoft.com>
2023-07-09 21:01:52 -0700

    first commit

---
+++ backend/Extensions/KeyVaultConfigurationBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace MinimalApi.Extensions;
+
+internal static class KeyVaultConfigurationBuilderExtensions
+{
+    internal static ConfigurationManager ConfigureAzureKeyVault(this ConfigurationManager builder)
+    {
+        var azureKeyVaultEndpoint = builder["AzureKeyVaultEndpoint"];
+        ArgumentNullException.ThrowIfNullOrEmpty(azureKeyVaultEndpoint);
+
+        builder.AddAzureKeyVault(
+            new Uri(azureKeyVaultEndpoint), new DefaultAzureCredential());
+
+        return builder;
+    }
+}
---
+++ backend/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace MinimalApi.Extensions;
+
+internal static class WebApplicationExtensions
+{
+    internal static WebApplication MapApi(this WebApplication app)
+    {
+        var api = app.MapGroup("api");
+
+
+        // Long-form chat w/ contextual history endpoint
+        api.MapPost("chat", OnPostChatAsync);
+
+
+
+        return app;
+    }
+
+    private static async Task<IResult> OnPostChatAsync(
+        ChatRequest request,
+        ReadRetrieveReadChatService chatService,
+        CancellationToken cancellationToken)
+    {
+        if (request is { History.Length: > 0 })
+        {
+            var response = await chatService.ReplyAsync(
+                request.History, request.Overrides, cancellationToken);
+
+            return TypedResults.Ok(response);
+        }
+
+        return Results.BadRequest();
+    }
+
+}
